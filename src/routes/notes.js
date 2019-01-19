c1a69700284c84d97fcfe0a08fa137835d15de37
Alonso Parra <alonsogparra@gmail.com>
2019-01-18 20:30:32 -0400

    Added to note routes the user id to make the notes unique as the user

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -31,6 +31,7 @@ router.post('/notes/new-note', isAuthenticated, async (req, res) => {
     }); 
   } else {
     const newNote = new Note({ title, description });
+    newNote.user = req.user.id;
     await newNote.save();
     req.flash('success_msg', 'Note added successfully');
     res.redirect('/notes');
@@ -40,7 +41,7 @@ router.post('/notes/new-note', isAuthenticated, async (req, res) => {
 
 // Get all Notes
 router.get('/notes', isAuthenticated, async (req, res) => {
-  const notes = await Note.find().sort({date: 'desc'});
+  const notes = await Note.find({ user: req.user.id }).sort({date: 'desc'});
   res.render('notes/all-notes', { notes });
 });
 
